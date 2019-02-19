d6128569d54a0fdc249b7876071908cab229f9c2
bab-graph <baptiste.couble@free.fr>
2019-02-19 12:41:42 +0100

    Assets folder for image and txt files + 3 PY files that will make up the labyrinth program

---
+++ constant.py
@@ -0,0 +1,23 @@
+"""Constants Game MacGyver"""
+
+# window parameter
+
+# sprite_number = 15
+# sprite_size = 30px
+# window_size = sprite_number * sprite_size
+
+# image list
+
+"""
+home
+background, path, road
+wall
+starting line
+finishing line
+object 0
+object 1
+object 2
+object 3
+MacGyver
+Guardian
+"""
---
+++ mgLabyrinth.py
@@ -0,0 +1,16 @@
+"""
+Project 3
+Maze game Mac Gyver 
+
+
+"""
+
+def main():
+	# Main loop
+		# Loop Game Menu
+			# end menu
+		# Loop Game
+			# end game
+
+if __name__ == "__main__":
+    main()
